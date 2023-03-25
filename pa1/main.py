9efa2946bda90ca0bfdbcd0d1f9e90950c31f295
wrathchild14 <jovanprodanov123@gmail.com>
2023-03-25 15:02:24 +0100

    Fix deprecated methods and refactor code

--- pa1/main.py
+++ pa1/main.py
@@ -1,26 +1,26 @@
-import requests
-import threading
-import psycopg2
-import urllib3
-import concurrent.futures
 import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 from urllib.parse import urldefrag
 
-#Properties to use selenium for proper JS rendering
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Properties to use selenium for proper JS rendering
 WEB_DRIVER_LOCATION = "webdriver/chromedriver.exe"
 TIMEOUT = 5
 chrome_options = Options()
 chrome_options.add_argument("user-agent=fri-wier-spoders")
-chrome_options.headless = True
+chrome_options.add_argument("--headless")
+
 
 class Frontier:
     def __init__(self, seed):
         self.frontier = list(seed)
         self.visited = set()
-    
+
     def is_empty(self):
         return len(self.frontier) == 0
 
@@ -44,7 +44,7 @@ class Frontier:
             self.frontier.append(url)
 
     def normalize_url(self, url):
-        #Remove #
+        # Remove #
         url = urldefrag(url)[0]
         url = str(url)
         # Remove trailing slash.
@@ -61,29 +61,30 @@ class Frontier:
     def print_frontier(self):
         print(self.frontier)
 
+
 def crawler(frontier):
-    driver = webdriver.Chrome(WEB_DRIVER_LOCATION, options=chrome_options)  
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     while not frontier.is_empty():
-        #Get URL from the frontier
-        currentUrl = frontier.pop_element()
-        #Retrieve page
-        driver.get(currentUrl)
+        # Get URL from the frontier
+        current_url = frontier.pop_element()
+        # Retrieve page
+        driver.get(current_url)
         # Timeout needed for Web page to render
         time.sleep(TIMEOUT)
         html = driver.page_source
-        #Parsing links
+        # Parsing links
         for element in driver.find_elements(By.TAG_NAME, 'a'):
             link = element.get_attribute("href")
             frontier.add_url(link)
-        #print(frontier.print_frontier())
+        # print(frontier.print_frontier())
 
     driver.close()
 
-if __name__ == '__main__':
 
-    #Seed urls to frontier
+if __name__ == '__main__':
+    # Seed urls to frontier
     seed_urls = ['http://gov.si', 'http://evem.gov.si', 'http://e-uprava.gov.si', 'http://e-prostor.gov.si']
     frontier = Frontier(seed_urls)
 
-    #Start crawler
+    # Start crawler
     crawler(frontier)
