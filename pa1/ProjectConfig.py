de59cd6b6eb75272f7e8ca77fe4b3df643ac49ce
Friday202 <122792037+Friday202@users.noreply.github.com>
2023-03-26 18:01:18 +0200

    Create ProjectConfig.py

---
+++ pa1/ProjectConfig.py
@@ -0,0 +1,14 @@
+# Using Chrome 111
+WEB_DRIVER_LOCATION = "webdriver/chromedriver.exe"
+
+TIMEOUT = 5
+PROJECT_NAME = 'fri-wier-spoders'
+
+NUMBER_OF_WORKERS = 1
+SEED_URLS = ['http://gov.si', 'http://evem.gov.si', 'http://e-uprava.gov.si', 'http://e-prostor.gov.si']
+
+DB_HOST = "localhost"
+DB_DATABASE = "postgresql-wier" 
+DB_USER = "User"
+DB_PASSWORD = "SecretPassword"
+
