76255b4e66127705d5b13960e843271c6f87e6b6
Friday202 <122792037+Friday202@users.noreply.github.com>
2023-03-30 22:35:03 +0200

    Added hash and multithreading locks

--- pa1/db_controller.py
+++ pa1/db_controller.py
@@ -5,69 +5,63 @@ lock = threading.Lock()
 
 
 class DatabaseController:
-    def __init__(self, host: str = "localhost", user: str = "user", password: str = "SecretPassword"):
+    def __init__(self, host="localhost", user="User", password="SecretPassword"):
         self.connection = psycopg2.connect(host=host, user=user, password=password)
         self.connection.autocommit = True
 
     # TABLE SITE
 
     def insert_site(self, domain: str, robots_content: str = '', sitemap_content: str = ''):
-        cur = self.connection.cursor()
-        cur.execute("INSERT INTO crawldb.site(domain, robots_content, sitemap_content)"
-                    "VALUES (%s, %s, %s)", (domain, robots_content, sitemap_content))
+        with lock: 
+            cur = self.connection.cursor()
+            cur.execute("INSERT INTO crawldb.site(domain, robots_content, sitemap_content)"
+                        "VALUES (%s, %s, %s)", (domain, robots_content, sitemap_content))
 
-        print(f"Log: inserted site with {domain} domain into database")
-        cur.close()
+            print(f"Log: inserted site with {domain} domain into database")
+            cur.close()
 
     def get_site(self, domain):
-        cur = self.connection.cursor()
-        cur.execute(
-            "SELECT * FROM crawldb.site WHERE domain=%s;",
-            (domain,)
-        )
-        # Check if array is empty, meaning we didn't find the site already present in the table
-        site_id = -1
-        result = cur.fetchall()
-        if result:
-            site_id = result[0][0]
+        with lock:
+            cur = self.connection.cursor()
+            cur.execute(
+                "SELECT * FROM crawldb.site WHERE domain=%s;",
+                (domain,)
+            )
+            # Check if array is empty, meaning we didn't find the site already present in the table
+            site_id = -1
+            result = cur.fetchall()
+            if result:
+                site_id = result[0][0]
 
-        cur.close()
-        return site_id
+            cur.close()
+            return site_id
 
     def get_robots_txt(self, domain):
-        cur = self.connection.cursor()
-        cur.execute(
-            "SELECT robots_content FROM crawldb.site WHERE domain=%s;",
-            (domain,)
-        )
-        # Check if array is empty, meaning we didn't find the site already present in the table
-        robots = None
-        result = cur.fetchall()
-        if result:
-            robots = result[0][0]
+        with lock:
+            cur = self.connection.cursor()
+            cur.execute(
+                "SELECT robots_content FROM crawldb.site WHERE domain=%s;",
+                (domain,)
+            )
+            # Check if array is empty, meaning we didn't find the site already present in the table
+            robots = None
+            result = cur.fetchall()
+            if result:
+                robots = result[0][0]
 
-        cur.close()
-        return robots
-        
-    # Obsolete
-    # def insert_robots_txt(self, domain, robots_content):
-    #     cur = self.connection.cursor()
-    #     cur.execute(
-    #         "INSERT INTO crawldb.site (domain, robots_content) VALUES (%s, %s)",
-    #         (domain, robots_content)
-    #     )
-    #     self.connection.commit()
-    #     cur.close()
+            cur.close()
+            return robots      
 
     # TABLE PAGE
 
     def insert_page(self, url: str, page_type_code: str, http_status_code: int, html_content: str = ''):
-        cur = self.connection.cursor()
-        cur.execute("INSERT INTO crawldb.page(url, page_type_code, html_content, http_status_code,accessed_time)"
-                    "VALUES (%s, %s, %s, %s, now())", (url, page_type_code, html_content, http_status_code))
+        with lock:
+            cur = self.connection.cursor()
+            cur.execute("INSERT INTO crawldb.page(url, page_type_code, html_content, http_status_code,accessed_time)"
+                        "VALUES (%s, %s, %s, %s, now())", (url, page_type_code, html_content, http_status_code))
 
-        print(f"Log: inserted page {url} with {page_type_code} type and {http_status_code} status into database")
-        cur.close()
+            print(f"Log: inserted page {url} with {page_type_code} type and {http_status_code} status into database")
+            cur.close()
 
     def print_pages(self):
         print(f"Log: Pages got from database")
@@ -80,33 +74,62 @@ class DatabaseController:
     # TABLE PAGE_DATA
 
     def insert_page_data(self, page_id, data_type_code, data):
-        cur = self.connection.cursor()
-        cur.execute("INSERT INTO crawldb.page_data(page_id, data_type_code, data)"
-                    "VALUES (%s, %s, %s)", (page_id, data_type_code, data))
+        with lock:
+            cur = self.connection.cursor()
+            cur.execute("INSERT INTO crawldb.page_data(page_id, data_type_code, data)"
+                        "VALUES (%s, %s, %s)", (page_id, data_type_code, data))
 
-        print(f"Log: inserted page_data for page {page_id} and {data_type_code} code into database")
-        cur.close()
+            print(f"Log: inserted page_data for page {page_id} and {data_type_code} code into database")
+            cur.close()
 
     # TABLE IMAGE
 
     def insert_image(self, page_id, filename, content_type, data, accessed_time):
-        cur = self.connection.cursor()
-        cur.execute("INSERT INTO crawldb.image(page_id, filename, content_type, data, accessed_time)"
-                    "VALUES (%s,%s,%s,%s,%s)", (page_id, filename, content_type, data, accessed_time))
+        with lock:
+            cur = self.connection.cursor()
+            cur.execute("INSERT INTO crawldb.image(page_id, filename, content_type, data, accessed_time)"
+                        "VALUES (%s,%s,%s,%s,%s)", (page_id, filename, content_type, data, accessed_time))
 
-        print(f"Log: inserted image {filename} with {content_type} type into database")
-        cur.close()
+            print(f"Log: inserted image {filename} with {content_type} type into database")
+            cur.close()
 
     # TABLE LINK
 
     def insert_link(self, from_page, to_page):
-        cur = self.connection.cursor()
-        cur.execute("INSERT INTO crawldb.link(from_page, to_page)"
-                    "VALUES (%s,%s)", (from_page, to_page))
+        with lock:
+            cur = self.connection.cursor()
+            cur.execute("INSERT INTO crawldb.link(from_page, to_page)"
+                        "VALUES (%s,%s)", (from_page, to_page))
 
-        print(f"Log: inserted image {filename} with {content_type} type into database")
-        cur.close()
+            print(f"Log: inserted image {filename} with {content_type} type into database")
+            cur.close()
+    
+    # HASHING CODE
+    
+    def is_duplicate(self, page_hash):
+        with lock:
+            cur = self.connection.cursor()
+            cur.execute(
+                "SELECT page_id FROM crawldb.hash WHERE hash=%s LIMIT 1;",
+                (page_hash,)
+            )
+            status = False
+            result = cur.fetchall()
+            if result:
+                status = True
+            cur.close()
+            return status
 
+    def insert_hash(self, page_id, page_hash):
+        with lock:
+            cur = self.connection.cursor()
+            cur.execute(
+                "INSERT INTO crawldb.hash (page_id, hash) VALUES ((SELECT id FROM crawldb.page WHERE id=%s),%s);",
+                (page_id, page_hash)
+            )
+            cur.close()
+            return
+    
     # CLOSE CONNECTION
     
     def close(self):
