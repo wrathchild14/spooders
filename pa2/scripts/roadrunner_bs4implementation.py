454c38be7cb4c22ee78b318966b15875db8a5ed6
wrathchild14 <jovanprodanov123@gmail.com>
2023-05-05 13:35:43 +0200

    fix/rework: wrapper generated in correctly ordered tags

--- pa2/scripts/roadrunner_bs4implementation.py
+++ pa2/scripts/roadrunner_bs4implementation.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup, Comment
+from bs4 import BeautifulSoup, Comment, NavigableString, Tag
 import re
 
 
@@ -11,51 +11,56 @@ def generate_wrapper(html1, html2):
     for tag in soup2(unwanted_tags):
         tag.decompose()
 
-    tags1 = set([tag.name for tag in soup1.find_all()])
-    tags2 = set([tag.name for tag in soup2.find_all()])
-    common_tags = list(tags1.intersection(tags2))
+    tags1 = soup1.find_all()
+    tags2 = soup2.find_all()
 
-    regexes = {}
-    for tag in common_tags:
-        regexes[tag] = generate_regex(tag, soup1, soup2)
+    regex = ""
+    for i, (tag1, tag2) in enumerate(zip(tags1, tags2)):
+        if tag1.name and tag2.name:
+            if isinstance(tag1, Comment):
+                continue
+            elif tag1.name != tag2.name:
+                regex += "<" + tag1.name + ".*?>.*?</" + tag1.name + ">\n"
+            else:
+                regex += "<" + tag1.name + get_common_attrs(tag1, tag2) + ">\n"
+                if tag1.string and tag1.string.strip():
+                    regex += re.escape(tag1.string.strip()) + "\n"
+                elif tag1.contents:
+                    regex += generate_regex(tag1, tag2, indent=2)
+                regex += "</" + tag1.name + ">\n"
 
-    for tag in common_tags:
-        regexes[tag] = refine_regex(tag, regexes[tag])
-
-    wrapper = ""
-    for tag in common_tags:
-        wrapper += regexes[tag] + "\n"
-    return wrapper, regexes
+    return regex, None
 
 
-def generate_regex(tag, soup1, soup2, indent=0):
-    tag1 = soup1.find(tag)
-    tag2 = soup2.find(tag)
+def generate_regex(tag1, tag2, indent=0):
     attrs = get_common_attrs(tag1, tag2)
 
-    if tag == 'div' and 'id' in tag1.attrs and 'class' in tag1.attrs and tag1.attrs['id'] == 'fb-root' \
-            and tag1.attrs['class'] == ['fb_reset']:
-        return ''
+    if attrs is None:
+        attrs = ""
+    if tag1.name:
+        regex = " " * indent + "<" + tag1.name + attrs + ">\n"
+        for child1, child2 in zip(tag1.children, tag2.children):
+            if child1.name and child2.name:
+                if isinstance(child1, Comment):
+                    continue
+                elif isinstance(child1, str) and child1.strip():
+                    regex += " " * (indent + 2) + re.escape(child1.strip()).replace("\\", "") + "\n"
+                # elif isinstance(child1, NavigableString) and child1.string.strip():
+                #     regex += " " * (indent + 2) + re.escape(child1.string.strip()).replace("\\", "") + "\n"
+                elif child1.name == child2.name:
+                    regex += generate_regex(child1, child2, indent=indent + 2)
+                else:
+                    regex += " " * (indent + 2) + "<" + child1.name + ".*?>.*?</" + child1.name + ">\n"
 
-    regex = " " * indent + "<" + tag + attrs + ">\n"
-    if not tag1.contents:
+        regex += " " * indent + "</" + tag1.name + ">\n"
+        return "\n".join([line for line in regex.split("\n") if line.strip()])
+    else:
         return ""
-
-    for child in tag1.contents:
-        if isinstance(child, Comment):
-            continue
-        if child.name:
-            regex += generate_regex(child.name, soup1, soup2, indent + 2)
-        else:
-            regex += " " * (indent + 2) + re.escape(str(child).strip()).replace("\\", "") + "\n"
-
-    regex += " " * indent + "</" + tag + ">\n"
-    return "\n".join([line for line in regex.split("\n") if line.strip()])
 
 
 def get_common_attrs(tag1, tag2):
-    attrs1 = tag1.attrs
-    attrs2 = tag2.attrs
+    attrs1 = tag1.attrs if isinstance(tag1, Tag) else {}
+    attrs2 = tag2.attrs if isinstance(tag2, Tag) else {}
     common_attrs = ""
     for key in attrs1.keys():
         if key in attrs2 and str(attrs1[key]) == str(attrs2[key]):
@@ -104,8 +109,8 @@ if __name__ == "__main__":
     wrapper_text, wrapper_dict = generate_wrapper(page_audi, page_volvo)
     print(wrapper_text)
 
-    data1 = extract_data(page_audi, wrapper_dict)
-    print("Data from page 1:", data1)
-
-    data2 = extract_data(page_volvo, wrapper_dict)
-    print("Data from page 2:", data2)
+    # data1 = extract_data(page_audi, wrapper_dict)
+    # print("Data from page 1:", data1)
+    #
+    # data2 = extract_data(page_volvo, wrapper_dict)
+    # print("Data from page 2:", data2)
